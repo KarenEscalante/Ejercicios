532ff7017485f1ba89ad74d8af1cd15e665275eb
USUARIO <USUARIO@DESKTOP-SG2O7VJ>
2021-09-12 22:27:23 -0500

    primero

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author KarenEscalante
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        var nombre1="maría";
+        var nombre2="paula";
+        var union=nombre1 + " " + nombre2;
+        System.out.println("union=" +union);
+        
+        
+        int a=2;
+        int e=3;
+        
+                System.out.println(a+e);
+                System.out.println(a+e+union);
+                System.out.println(union+a+e);
+                System.out.println(union+(a+e));
+                        
+        
+        int numero1=8;
+        int numero2=numero1;
+        System.out.println("la variable numero 1 vale: " +numero1);
+        System.out.println("La variable numero 2 vale: " +numero2);
+        
+        
+        
+        long numer1=8;
+        int numer2=13;
+        long resta= numer1-numer2;
+        System.out.println("el resultado de la resta es: " +resta);
+        
+        
+        int nmro1=3;
+        int nmro2=52;
+        int suma = nmro1+nmro2;
+        System.out.println("El resultado de la suma es: " +suma);
+        
+        
+        double número1= 7.6;
+        int número2= 4;
+        double multiplicacion = número1*número2;
+        System.out.println("El resultado de la multiplicacion es: " +multiplicacion);
+        
+        
+        
+        float nume1 =2.3F;
+        int nume2 =5;
+        float division = nume1/nume2;
+                System.out.println("El resultado de la division es: " +division);
+                
+                
+                
+                
+     
+                
+                
+              
+                
+                
+                        
+                
+                
+                
+                        
+                        
+        
+        
+        
+        
+        
+        
+    }
+   
+    
+    
+}
